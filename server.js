772989424e89798e93d3323eab1b94c7808db0ac
Nick Yushenko <nickita.yushchenko@prodamus.ru>
2024-02-21 01:54:38 +0300

    remove duplicates

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-
-const PORT = 3000
-const app = express()
-app.use(express.static('./dist'))
-
-app.listen(PORT, () => {
-  console.log(`Server works on ${PORT}`)
-})
