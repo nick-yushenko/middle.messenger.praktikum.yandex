8676a55f84b65087d77b6fc721c6d32fcec7f142
Nick Yushenko <nickita.yushchenko@prodamus.ru>
2024-03-06 07:50:22 +0300

    fix start by npm run start

--- src/js/server.js
+++ src/js/server.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = 3000;
 
 app.use(express.static("./dist"));
-
+console.log(express.static("./dist").static)
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
 });
